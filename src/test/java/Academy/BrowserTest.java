7114d192e53387733d0631ba2f52af681d369c67
kothas123 <102431568+kothas123@users.noreply.github.com>
2026-09-10 11:30:58 +0530

    Update BrowserTest.java

--- src/test/java/Academy/BrowserTest.java
+++ src/test/java/Academy/BrowserTest.java
@@ -1,30 +1,38 @@
 package Academy;
 
-import static org.junit.Assert.assertEquals;
-
 import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-
-
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class BrowserTest {
 
-	
-	@Test
-	public void getData()
-	{
-		System.out.println("Hello Guys");
-		WebDriver driver =new ChromeDriver();
-		driver.get("https://rahylshettyacademy.azurewebsites.net/webapp/");
-		String text =driver.findElement(By.cssSelector("h1")).getText();
-		System.out.println(text);
-		Assert.assertTrue(text.equalsIgnoreCase("RahulShettyAcademy.com Learning"));
-		driver.close();
-	
-		
-		
-	}
+    @Test
+    public void getData() {
+
+        System.out.println("Hello Guys");
+
+        ChromeOptions options = new ChromeOptions();
+
+        options.addArguments("--headless=new");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--window-size=1920,1080");
+
+        WebDriver driver = new ChromeDriver(options);
+
+        driver.get("https://rahylshettyacademy.azurewebsites.net/webapp/");
+
+        String text = driver.findElement(By.cssSelector("h1")).getText();
+
+        System.out.println(text);
+
+        Assert.assertTrue(
+            text.equalsIgnoreCase("RahulShettyAcademy.com Learning")
+        );
+
+        driver.quit();
+    }
 }
